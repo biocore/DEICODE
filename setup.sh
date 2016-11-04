1e2f70a20c58b5a2f96b41b8cadaf3641326ede3
Cameron Martino <cameronmartino@gmail.com>
2016-11-04 09:43:02 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@ dir=$(pwd)
 #conda env create -f environment.yml
 git clone https://github.com/dganguli/robust-pca.git $dir/r_pca
 
-echo Done! Please activate the envrionment COD_env and navigate to the jupyter notebooks
-echo To source activate the envrionment execute:$ source activate COD_env
+echo Done! Please activate the envrionment DEICODE_env and navigate to the jupyter notebooks
+echo To source activate the envrionment execute:$ source activate DEICODE_env
