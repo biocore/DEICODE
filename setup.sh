cbf212ae06fa5597ad3d3cdbd3a008174cbf663e
Cameron Martino <cameronmartino@gmail.com>
2017-01-06 10:56:39 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@
 dir=$(pwd)
 conda env create -f environment.yml
 git clone https://github.com/dganguli/robust-pca.git $dir/r_pca
+chmod +x DEICODE.py
 
 echo Done! Please activate the envrionment DEICODE_env and navigate to the jupyter notebooks
 echo To source activate the envrionment execute:$ source activate DEICODE_env
