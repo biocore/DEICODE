615159c2a7d39fee64c9eacc9621ca3722bf54da
Cameron Martino <cameronmartino@gmail.com>
2017-04-17 15:21:05 -0700

    Add files via upload

--- Impute_vis.py
+++ Impute_vis.py
@@ -1,10 +1,10 @@
 import brewer2mpl
 import matplotlib
 import pandas as pd
-from wpca import WPCA, EMPCA
+from sklearn.decomposition import PCA
+import seaborn as sns
 import matplotlib.pyplot as plt
 from gneiss.util import match
-
 
 class PCA_niche(object):
 
@@ -12,7 +12,7 @@ class PCA_niche(object):
     This contains visuals for DEICODE
     '''''
 
-    def niche_visual(otu,sfi,tax_index,bact_to_show,niche_plot,Pc_plot,mappingdf,weight):
+    def niche_visual(otu,sfi,tax_index,bact_to_show,niche_plot,Pc_plot,mappingdf):
         
 
         #imputed
@@ -31,9 +31,9 @@ class PCA_niche(object):
         plot_var_otuch=plot_var_otuch.groupby(plot_var_otuch.index).sum() #group by name
         plot_var_otuch=plot_var_otuch.T #transpose for PCA
         
-        pca_model=WPCA(n_components=3) #WPCA
+        pca_model=PCA(n_components=3) #WPCA
         X2=plot_var_otu.as_matrix() #pd to matrix
-        X_reduced_var = pca_model.fit_transform(X2,weight) #transform
+        X_reduced_var = pca_model.fit_transform(X2) #transform
         pccompdf = pd.DataFrame(pca_model.components_,columns=plot_var_otu.columns,index = ['PC-1','PC-2','PC-3']).T #get wieghts
         pccompdf.sort_values([Pc_plot], ascending = [False], inplace = True) #sort weights
         highest_var_bact=list(pccompdf.index.values) # re order table by wieghts
@@ -80,25 +80,51 @@ class PCA_niche(object):
         return out_niche_linkeddf,observed_table_sfi,index_mean,index_std,highest_var_bact,pccompdf
 
 
-    def plot_niche(out_niche_linkeddf,observed_table_sfi,mappingdf,encoded_mapping,niche_plot,Pc_plot,index_mean,index_std,le,cont,weight):
+    def plot_niche(out_niche_linkeddf,observed_table_sfi,mappingdf,encoded_mapping,niche_plot,Pc_plot,index_mean,index_std,le,cont):
 
-        #WPCA
+        #PCA
         observed_table_sfi, mapping = match(observed_table_sfi.T, mappingdf)
-        pca_model=WPCA(n_components=2)
+        pca_model=PCA(n_components=2)
         X2=observed_table_sfi.as_matrix()
-        X_reduced2 = pca_model.fit_transform(X2,weight)
+        X_reduced2 = pca_model.fit_transform(X2)
         
     
         if Pc_plot=="PC-1":
         
             if cont==True:
                 
-                fig, (ax1, ax2) = plt.subplots(ncols=1, nrows=2, figsize=(30, 15),sharey=False)
-                out_niche_linkeddf.T[index_mean].plot(kind='bar',width=.78, yerr=out_niche_linkeddf.T[index_std].values.T, alpha = 0.92,error_kw=dict(elinewidth=1,capsize=2,barsabove=True,ecolor='k',ms=1, mew=1),rot=0,colormap="Set1",sharey=True,fontsize=15,ax=ax1)
+                index_mean_new=[]
+                index_std_new=[]
+                new_taxa=[]
+                #lowest taxaclassification + (phylum)
+                for nm in out_niche_linkeddf.index.values.tolist():
+                    #find lowest taxa level
+                    q=0
+                    while q<(len(nm.split(';'))-1):
+                        lw=nm.split(';')[q]
+                        q+=1
+                        if len(nm.split(';')[q])==3:
+                            break
+                    if '[' in lw.split('__')[1]:
+                        lw=lw.split('__')[1]
+                        lw=lw[1:-1]
+                    else:
+                        lw=lw.split('__')[1]
+
+                    if 'std' in nm:
+                        index_std_new.append(lw+' ('+nm.split(';')[0].split('__')[1]+')'+'std')
+                        new_taxa.append(lw+' ('+nm.split(';')[0].split('__')[1]+')'+'std')
+                    else:
+                        index_mean_new.append(lw+' ('+nm.split(';')[0].split('__')[1]+')')
+                        new_taxa.append(lw+' ('+nm.split(';')[0].split('__')[1]+')')
+                out_niche_linkeddf.index=new_taxa
+                #plot
+                fig, (ax1, ax2) = plt.subplots(ncols=1, nrows=2, figsize=(20, 18),sharey=False)
+                out_niche_linkeddf.T[index_mean_new[:-5]].plot(kind='area',lw=6, alpha = 0.92,rot=0,colormap="Set1",sharey=True,fontsize=15,ax=ax1)
                 ax1.legend(loc=2,prop={'size':16},bbox_to_anchor=(1.0, 1.0))
                 Y=encoded_mapping[niche_plot][0].tolist()
-                ax2.scatter(X_reduced2[:, 0], X_reduced2[:, 1], c=list(encoded_mapping[niche_plot][0]),cmap=plt.cm.cool,s=200)
-                p=ax2.scatter(X_reduced2[:, 0], X_reduced2[:, 1], c=list(encoded_mapping[niche_plot][0]),cmap=plt.cm.cool,s=200)
+                ax2.scatter(X_reduced2[:, 0], X_reduced2[:, 1], c=list(list(mappingdf[niche_plot])),cmap=plt.cm.cool,s=200)
+                p=ax2.scatter(X_reduced2[:, 0], X_reduced2[:, 1], c=list(list(mappingdf[niche_plot])),cmap=plt.cm.cool,s=200)
                 fig.colorbar(p,orientation='horizontal')
                 # Set common labels
                 ax1.set_xlabel(('Mean Frequency in %s Niche'%niche_plot))
@@ -109,55 +135,48 @@ class PCA_niche(object):
                 ax2.set_title(('PCA on Fully Dense Imputed OTU Table (colored by %s)'%niche_plot))
                 fig.set_tight_layout(True)
                 return plt
-            
+
             if cont==False:
                 
-                fig, (ax1, ax2) = plt.subplots(ncols=1, nrows=2, figsize=(30, 15),sharey=False)
-                
-                #bar
-                out_niche_linkeddf.T[index_mean].plot(kind='bar',width=.78, yerr=out_niche_linkeddf.T[index_std].values.T, alpha = 0.92,error_kw=dict(elinewidth=1,capsize=2,barsabove=True,ecolor='k',ms=1, mew=1),rot=0,colormap="Set1",sharey=True,fontsize=30,ax=ax1)
-                ax1.legend(loc=2,prop={'size':25},bbox_to_anchor=(1.0, 1.0))
-                Y=mapping[niche_plot].tolist()
-                
-                #scatter (with legend for non coninous data)
-                Y=list(map(int, encoded_mapping[niche_plot][0]))
-                Y2=list(encoded_mapping[niche_plot][1])
-                bmap7 = brewer2mpl.get_map('Set1','qualitative',9,reverse=True)
-                colors = bmap7.mpl_colors
-                handles, labels = ax2.get_legend_handles_labels()
-                ax2.legend(handles, labels)
-                scatter_proxy=[]
-                k=set(Y2)
-                #qlists
-                q1=[0]
-                q2=[0,8]
-                q3=[0,4,8]
-                q4=[0,3,5,8]
-                q5=[0,2,4,6,8]
-                q6=[0,1,3,5,6,8]
-                q7=[0,2,3,5,6,7,8]
-                q8=[0,1,2,3,4,5,7,8]
-                q9=[0,1,2,3,4,5,6,7,8]
-                choodict={1: q1,2: q2,3: q3,4: q4,5: q5,6: q6,7: q7,8: q8,9: q9}
-                #choose qlist
-                for key,value in choodict.items():
-                    if key==int((len(set(Y2)))):
-                        for q in value:
-                            scatter_proxy.append(matplotlib.lines.Line2D([0],[0], linestyle="none", c=colors[q],markersize=30, marker = 'o'))
-                
-                if int(len(k))>=3:
-                    numrows=4
-                else:
-                    numrows=int(len(k))
-                ax2.legend(scatter_proxy, k, numpoints = 1,loc=2,prop={'size':40},bbox_to_anchor=(1.0, 1.0),fontsize = 'large', labelspacing=.1, borderaxespad=0.)
-                ax2.scatter(X_reduced2[:, 0], X_reduced2[:, 1], c=Y,cmap=plt.cm.Set1_r,s=350)
-                # Set common labels
-                ax1.set_xlabel(('Mean Frequency in %s Niche'%niche_plot), fontsize=20)
-                ax1.set_ylabel('Niche (mean frequency)', fontsize=20)
-                ax2.set_xlabel('PC-1', fontsize=20)
-                ax2.set_ylabel('PC-2', fontsize=20)
-                ax1.set_title(('OTUs With Highest Weighted Varaince in %s'%niche_plot), fontsize=35)
-                ax2.set_title(('PCA on Fully Dense Imputed OTU Table (colored by %s)'%niche_plot), fontsize=35)
+                ids=['PC1','PC2']
+                spPCAplot=pd.DataFrame(X_reduced2,observed_table_sfi.index.values,ids)
+                spPCAplot['labels']=mappingdf[niche_plot]
+                #clean
+                index_mean_new=[]
+                index_std_new=[]
+                new_taxa=[]
+                #lowest taxaclassification + (phylum)
+                for nm in out_niche_linkeddf.index.values.tolist():
+                    #find lowest taxa level
+                    q=0
+                    while q<(len(nm.split(';'))-1):
+                        lw=nm.split(';')[q]
+                        q+=1
+                        if len(nm.split(';')[q])==3:
+                            break
+                    if '[' in lw.split('__')[1]:
+                        lw=lw.split('__')[1]
+                        lw=lw[1:-1]
+                    else:
+                        lw=lw.split('__')[1]
+                    
+                    if 'std' in nm:
+                        index_std_new.append(lw+' ('+nm.split(';')[0].split('__')[1]+')'+'std')
+                        new_taxa.append(lw+' ('+nm.split(';')[0].split('__')[1]+')'+'std')
+                    else:
+                        index_mean_new.append(lw+' ('+nm.split(';')[0].split('__')[1]+')')
+                        new_taxa.append(lw+' ('+nm.split(';')[0].split('__')[1]+')')
+                out_niche_linkeddf.index=new_taxa
+                #plot
+                fig, (ax1, ax2) = plt.subplots(ncols=1, nrows=2, figsize=(20, 18),sharey=False)
+                #scatter
+                sns.swarmplot(x="PC1", y="PC2", data=spPCAplot, hue="labels", size=10,ax=ax1)
+                ax1.set_xlabel('$PC-1$')
+                ax1.set_ylabel('$PC-2$')
+                ax1.legend(loc=2,prop={'size':22},bbox_to_anchor=(.59, 1.03))
+                #barh
+                out_niche_linkeddf.T[index_mean_new].plot(kind='barh',width=.9, xerr=out_niche_linkeddf.T[index_std_new].values.T, alpha = 0.92,error_kw=dict(elinewidth=1,capsize=2,barsabove=True,ecolor='k',ms=1, mew=1),rot=0,colormap="Set1",sharey=True,fontsize=15,ax=ax2)
+                ax2.legend(loc=2,prop={'size':16},bbox_to_anchor=(1.0, 1.0))
                 fig.set_tight_layout(True)
                 return plt
 
@@ -166,69 +185,89 @@ class PCA_niche(object):
 
             if cont==True:
     
-                fig, (ax1, ax2) = plt.subplots(ncols=1, nrows=2, figsize=(30, 15),sharey=False)
-                out_niche_linkeddf.T[index_mean].plot(kind='bar',width=.78, yerr=out_niche_linkeddf.T[index_std].values.T, alpha = 0.92,error_kw=dict(elinewidth=1,capsize=2,barsabove=True,ecolor='k',ms=1, mew=1),rot=0,colormap="Set1",sharey=True,fontsize=15,ax=ax1)
+                index_mean_new=[]
+                index_std_new=[]
+                new_taxa=[]
+                #lowest taxaclassification + (phylum)
+                for nm in out_niche_linkeddf.index.values.tolist():
+                    #find lowest taxa level
+                    q=0
+                    while q<(len(nm.split(';'))-1):
+                        lw=nm.split(';')[q]
+                        q+=1
+                        if len(nm.split(';')[q])==3:
+                            break
+                    if '[' in lw.split('__')[1]:
+                        lw=lw.split('__')[1]
+                        lw=lw[1:-1]
+                    else:
+                        lw=lw.split('__')[1]
+                    
+                    if 'std' in nm:
+                        index_std_new.append(lw+' ('+nm.split(';')[0].split('__')[1]+')'+'std')
+                        new_taxa.append(lw+' ('+nm.split(';')[0].split('__')[1]+')'+'std')
+                    else:
+                        index_mean_new.append(lw+' ('+nm.split(';')[0].split('__')[1]+')')
+                        new_taxa.append(lw+' ('+nm.split(';')[0].split('__')[1]+')')
+                out_niche_linkeddf.index=new_taxa
+                #plot
+                fig, (ax1, ax2) = plt.subplots(ncols=1, nrows=2, figsize=(20, 18),sharey=False)
+                out_niche_linkeddf.T[index_mean_new[:-5]].plot(kind='area',lw=6, alpha = 0.92,rot=0,colormap="Set1",sharey=True,fontsize=15,ax=ax1)
                 ax1.legend(loc=2,prop={'size':16},bbox_to_anchor=(1.0, 1.0))
                 Y=encoded_mapping[niche_plot][0].tolist()
-                ax2.scatter(X_reduced2[:, 1], X_reduced2[:, 0], c=list(encoded_mapping[niche_plot][0]),cmap=plt.cm.cool,s=200)
-                p=ax2.scatter(X_reduced2[:, 1], X_reduced2[:, 0], c=list(encoded_mapping[niche_plot][0]),cmap=plt.cm.cool,s=200)
+                ax2.scatter(X_reduced2[:, 0], X_reduced2[:, 1], c=list(list(mappingdf[niche_plot])),cmap=plt.cm.cool,s=200)
+                p=ax2.scatter(X_reduced2[:, 0], X_reduced2[:, 1], c=list(list(mappingdf[niche_plot])),cmap=plt.cm.cool,s=200)
                 fig.colorbar(p,orientation='horizontal')
                 # Set common labels
-                ax1.set_ylabel(('Mean Frequency in %s Niche'%niche_plot))
-                ax1.set_xlabel('Niche')
-                ax2.set_xlabel('PC-2')
-                ax2.set_ylabel('PC-1')
+                ax1.set_xlabel(('Mean Frequency in %s Niche'%niche_plot))
+                ax1.set_ylabel('Niche')
+                ax2.set_xlabel('PC-1')
+                ax2.set_ylabel('PC-2')
                 ax1.set_title(('OTUs With Highest Weighted Varaince in %s'%niche_plot))
                 ax2.set_title(('PCA on Fully Dense Imputed OTU Table (colored by %s)'%niche_plot))
                 fig.set_tight_layout(True)
                 return plt
-            
+
             if cont==False:
                 
-                fig, (ax1, ax2) = plt.subplots(ncols=1, nrows=2, figsize=(30, 15),sharey=False)
-                
-                #bar
-                out_niche_linkeddf.T[index_mean].plot(kind='bar',width=.78, yerr=out_niche_linkeddf.T[index_std].values.T, alpha = 0.92,error_kw=dict(elinewidth=1,capsize=2,barsabove=True,ecolor='k',ms=1, mew=1),rot=0,colormap="Set1",sharey=True,fontsize=30,ax=ax1)
-                ax1.legend(loc=2,prop={'size':25},bbox_to_anchor=(1.0, 1.0))
-                Y=mapping[niche_plot].tolist()
-                #scatter (with legend for non coninous data)
-                Y=list(map(int, encoded_mapping[niche_plot][0]))
-                Y2=list(encoded_mapping[niche_plot][1])
-                bmap7 = brewer2mpl.get_map('Set1','qualitative',9,reverse=True)
-                colors = bmap7.mpl_colors
-                handles, labels = ax2.get_legend_handles_labels()
-                ax2.legend(handles, labels)
-                scatter_proxy=[]
-                k=set(Y2)
-                #qlists
-                q1=[0]
-                q2=[0,8]
-                q3=[0,4,8]
-                q4=[0,3,5,8]
-                q5=[0,2,4,6,8]
-                q6=[0,1,3,5,6,8]
-                q7=[0,2,3,5,6,7,8]
-                q8=[0,1,2,3,4,5,7,8]
-                q9=[0,1,2,3,4,5,6,7,8]
-                choodict={1: q1,2: q2,3: q3,4: q4,5: q5,6: q6,7: q7,8: q8,9: q9}
-                #choose qlist
-                for key,value in choodict.items():
-                    if key==int((len(set(Y2)))):
-                        for q in value:
-                            scatter_proxy.append(matplotlib.lines.Line2D([0],[0], linestyle="none", c=colors[q],markersize=30, marker = 'o'))
-                
-                if int(len(k))>=3:
-                    numrows=4
-                else:
-                    numrows=int(len(k))
-                ax2.legend(scatter_proxy, k, numpoints = 1,loc=2,prop={'size':40},bbox_to_anchor=(1.0, 1.0),fontsize = 'large', labelspacing=.1, borderaxespad=0.)
-                ax2.scatter(X_reduced2[:, 1], X_reduced2[:, 0], c=Y,cmap=plt.cm.Set1_r,s=350)
-                # Set common labels
-                ax1.set_ylabel(('Mean Frequency in %s Niche'%niche_plot), fontsize=20)
-                ax1.set_xlabel('Niche', fontsize=20)
-                ax2.set_xlabel('PC-2', fontsize=20)
-                ax2.set_ylabel('PC-1', fontsize=20)
-                ax1.set_title(('OTUs With Highest Weighted Varaince in %s'%niche_plot), fontsize=35)
-                ax2.set_title(('PCA on Fully Dense Imputed OTU Table (colored by %s)'%niche_plot), fontsize=35)
+                ids=['PC1','PC2']
+                spPCAplot=pd.DataFrame(X_reduced2,observed_table_sfi.index.values,ids)
+                spPCAplot['labels']=mappingdf[niche_plot]
+                #clean
+                index_mean_new=[]
+                index_std_new=[]
+                new_taxa=[]
+                #lowest taxaclassification + (phylum)
+                for nm in out_niche_linkeddf.index.values.tolist():
+                    #find lowest taxa level
+                    q=0
+                    while q<(len(nm.split(';'))-1):
+                        lw=nm.split(';')[q]
+                        q+=1
+                        if len(nm.split(';')[q])==3:
+                            break
+                    if '[' in lw.split('__')[1]:
+                        lw=lw.split('__')[1]
+                        lw=lw[1:-1]
+                    else:
+                        lw=lw.split('__')[1]
+
+                    if 'std' in nm:
+                        index_std_new.append(lw+' ('+nm.split(';')[0].split('__')[1]+')'+'std')
+                        new_taxa.append(lw+' ('+nm.split(';')[0].split('__')[1]+')'+'std')
+                    else:
+                        index_mean_new.append(lw+' ('+nm.split(';')[0].split('__')[1]+')')
+                        new_taxa.append(lw+' ('+nm.split(';')[0].split('__')[1]+')')
+                out_niche_linkeddf.index=new_taxa
+                #plot
+                fig, (ax1, ax2) = plt.subplots(ncols=2, nrows=1, figsize=(13, 8),sharey=False)
+                #scatter
+                sns.swarmplot(x="PC1", y="PC2", data=spPCAplot, hue="labels", size=10,ax=ax1)
+                ax1.set_xlabel('$PC-1$')
+                ax1.set_ylabel('$PC-2$')
+                ax1.legend(loc=2,prop={'size':22},bbox_to_anchor=(.59, 1.03))
+                #barh
+                out_niche_linkeddf.T[index_mean_new].plot(kind='barh',width=.9, xerr=out_niche_linkeddf.T[index_std_new].values.T, alpha = 0.92,error_kw=dict(elinewidth=1,capsize=2,barsabove=True,ecolor='k',ms=1, mew=1),rot=0,colormap="Set1",sharey=True,fontsize=15,ax=ax2)
+                ax2.legend(loc=2,prop={'size':16},bbox_to_anchor=(1.0, 1.0))
                 fig.set_tight_layout(True)
                 return plt
