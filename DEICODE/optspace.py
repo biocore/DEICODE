Tmp fix for numerical issues

DEICODE/optspace.py
@@ -16,7 +16,7 @@ def optspace(M_E, r, niter, tol):
     """
     E = M_E > 0
 
-    rescal_param = np.sqrt( np.count_nonzero(E) * r / norm(M_E,'fro') ** 2 ) ;
+    rescal_param = np.sqrt( np.count_nonzero(E) * r / norm(M_E, 'fro') ** 2 ) ;
     M_E = M_E * rescal_param ;
     # TODO: Add in trimming for rows and columns
     X0, S0, Y0 = svds(M_E, r)
@@ -31,12 +31,11 @@ def optspace(M_E, r, niter, tol):
     m0 = 10000
 
     rho = 0
-
     X, Y = X0, Y0.T;
 
     S = getoptS(X, Y, M_E, E);
     ft = M_E - X.dot(S).dot(Y.T)
-    dist = np.zeros(niter)
+    dist = np.zeros(niter+1)
     dist[0] = norm( np.multiply(ft, E) ,'fro')/ np.sqrt(nnz)
     for i in range(niter):
         W, Z = gradF_t(X, Y, S, M_E, E, m0, rho);
@@ -50,12 +49,11 @@ def optspace(M_E, r, niter, tol):
 
         # Compute the distortion
         ft = M_E - X.dot(S).dot(Y.T)
-        dist[i+1] = norm( np.multiply(ft, E) ,'fro')/ np.sqrt(nnz)
+        dist[i+1] = norm( np.multiply(ft, E) ,'fro') /  np.sqrt(nnz)
         if( dist[i+1] < tol ):
             break ;
     S = S /rescal_param ;
     return X, S, Y, dist
-
 
 def F_t(X, Y, S, M_E, E, m0, rho):
     """
@@ -68,7 +66,9 @@ def F_t(X, Y, S, M_E, E, m0, rho):
     M ~ XSY
     """
     n, r = X.shape
-    out1 = np.sum(np.sum((np.multiply((X.dot(S).dot(Y.T) - M_E), E)**2)))  /2
+    out1 = np.sum(
+        np.sum((
+            np.multiply((X.dot(S).dot(Y.T) - M_E), E)**2))) / 2
     out2 =  rho * G(Y, m0, r)
     out3 =  rho * G(X, m0, r)
     out = out1 + out2 + out3
@@ -102,11 +102,10 @@ def gradF_t(X, Y, S, M_E, E, m0, rho):
     YS = Y.dot(S.T)
     XSY = XS.dot(Y.T)
 
-    Qx = X.T.dot( np.multiply((M_E - XSY), E )).dot(YS) / n;
-    Qy = Y.T.dot( np.multiply((M_E - XSY), E ).T).dot(XS) / m;
-
-    W = np.multiply((XSY - M_E),E ).dot(YS) + X.dot(Qx) + rho*Gp(X,m0,r);
-    Z = np.multiply((XSY - M_E),E ).T.dot(XS) + Y.dot(Qy) + rho*Gp(Y,m0,r);
+    Qx = X.T.dot( np.multiply((M_E - XSY), E)).dot(YS) / n;
+    Qy = Y.T.dot( np.multiply((M_E - XSY), E).T).dot(XS) / m;
+    W = np.multiply((XSY - M_E), E).dot(YS) + X.dot(Qx) + rho * Gp(X, m0, r);
+    Z = np.multiply((XSY - M_E), E).T.dot(XS) + Y.dot(Qy) + rho * Gp(Y, m0, r);
     return W, Z
 
 def Gp(X, m0, r):
@@ -126,32 +125,40 @@ def getoptT(X, W, Y, Z, S, M_E, E, m0, rho):
     """ X, W, Y, Z, S, M_E, E, m0, rho """
     norm2WZ = norm(W, 'fro')**2 + norm(Z, 'fro')**2
 
-    # wtf does 20 come from?
-    # can we tune this?
+    # this is the resolution limit (t > 2**-20
     n_intervals = 20
     f = np.zeros(n_intervals+1)
     f[0] = F_t(X, Y, S, M_E, E, m0, rho)
     t = -1e-1
 
     for i in range(n_intervals):
+
         f[i+1] = F_t(X+t*W, Y+t*Z, S, M_E, E, m0, rho)
-        if( f[i+1] - f[1] <= .5*t*norm2WZ ):
+        if( (f[i+1] - f[1]) <= .5*t*norm2WZ ):
             return t
         t = t/2
     return t
 
+
+def leastsq(A, B):
+    """ Solves the problem Ax = B """
+    inv = np.linalg.pinv(np.dot(A.T, A))
+    cross = np.dot(inv, A.T)
+    x = np.dot(cross, B)
+    return x
+
+
 def getoptS(X, Y, M_E, E):
     """ X, Y, M_E, E """
     n, r = X.shape
-
     C = np.ravel(X.T.dot(M_E).dot(Y))
     A = np.zeros((r*r, r*r))
     for i in range(r):
         for j in range(r):
             ind = j*r + i
-            temp = X.T.dot(np.multiply(X[:, i].dot(Y[:, j].T), E )).dot(Y)
+            temp = np.multiply(X[:, i].dot(Y[:, j].T), E)
+            temp = X.T.dot(temp).dot(Y)
             A[:, ind] = np.ravel(temp)
-
-    S = np.linalg.lstsq(A, C)[0]
+    S = leastsq(A, C)
     S = S.reshape((r, r))
     return S

DEICODE/tests/test_optspace.py
@@ -1,5 +1,7 @@
 from DEICODE.optspace import G, F_t, gradF_t, Gp, getoptT, getoptS, optspace
 import numpy as np
+from numpy.random import randn, rand
+from numpy.linalg import norm
 import unittest
 import numpy.testing as npt
 from gneiss.util import block_diagonal
@@ -107,18 +109,62 @@ class TestOptspace(unittest.TestCase):
         # TODO: Use rectangular input matrices to test dimensions
         pass
 
-    def test_optspace(self):
-        ncols, nrows, nblocks = 10, 10, 2
+    def test_optspace_no_noise_small(self):
         np.random.seed(0)
-        X_noise = block_diagonal(ncols, nrows, nblocks)
-        rng = np.random.RandomState(0)
-        spar = 4
-        mask = np.random.randint(0, spar, size=X_noise.shape).astype(np.bool)
-        rand_zeros = np.random.rand(*X_noise.shape)*0
-        X_noise[mask] = rand_zeros[mask]
 
-        res = optspace(X_noise, r=2, niter=10, tol=1e-8)
-        print(res)
+        n = 10
+        m = 10
+        r = 3
+        tol = 1e-8
+
+        eps = r*np.log10(n);
+
+        U = randn(n, r)
+        V = randn(m, r)
+        Sig = np.eye(r)
+        M0 = U.dot(Sig).dot(V.T)
+
+        E = 1 - np.ceil( rand(n, m) - eps/np.sqrt(m*n)  )
+        M_E = np.multiply(M0, E)
+        X, S, Y, dist = optspace(M_E, r=3, niter=10, tol=tol)
+        exp_X = np.array([[-8.79865705e-01, 5.80137539e-01, -2.11945417e+00],
+                          [2.41254717e+00, -4.15135158e-02, -8.08590887e-02],
+                          [9.54706347e-01, 6.53315999e-01, -7.83143452e-01],
+                          [-1.26239471e-01, -1.15034917e-01, -6.73104191e-02],
+                          [-4.75788895e-04, -1.83763198e-03, 1.77844164e-03],
+                          [9.86004154e-01, 1.38993763e+00, 3.02042515e-01],
+                          [4.30368032e-03, 1.66220416e-02, -1.60866433e-02],
+                          [8.92420611e-01, 3.16881628e-01, 2.81385102e-01],
+                          [1.72236780e-01, 1.34451853e+00, -1.60799313e+00],
+                          [-8.24468845e-01, 2.31976548e+00, 1.45849936e+00]])
+
+        exp_S = np.array([[0.02717799, -0.00434587, -0.01896239],
+                          [-0.23599261, -0.05858438, -0.00468417],
+                          [0.20921023, -0.00193656, -0.03990232]])
+
+        exp_Y = np.array([[0.13823077, -0.02226359, 0.16660621],
+                          [-0.16332081, -1.83297345, 2.44195478],
+                          [0.48775225, 0.12243485, 0.09789257],
+                          [0.77352332, -1.52764724, -0.86834177],
+                          [1.10757851, 1.45958719, 1.71390276],
+                          [-0.42845364, -0.85749063, -0.28152323],
+                          [-2.41851741, -0.17623352, 0.19516526],
+                          [1.360709, -1.1687075, -0.39642448],
+                          [-0.03081843, -0.1683746, 0.18097424],
+                          [-0.07726637, -0.00677196, 0.02805902]])
+
+        exp_dist = np.array([0.87510604, 0.87510604, 0.87510604, 0.87510604,
+                             0.87510604, 0.87510603, 0.87510603, 0.87510603,
+                             0.87510603, 0.87510603, 0.87510603])
+
+        npt.assert_allclose(X, exp_X, atol=1e-5)
+        npt.assert_allclose(S, exp_S, atol=1e-5)
+        npt.assert_allclose(Y, exp_Y, atol=1e-5)
+        npt.assert_allclose(dist, exp_dist, atol=1e-5)
+
+    def test_optspace_noisy_small(self):
+        # add some noise
+        pass
 
     def test_optspace_rect(self):
         # TODO: Make the rows and columns different.