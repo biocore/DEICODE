bf69e5468ac3f980e4297aa3d1d514f57082818b
cameron martino <cameronmartino@camerons-MacBook-Pro.local>
2018-09-28 16:26:55 -0700

    flake

--- deicode/preprocessing.py
+++ deicode/preprocessing.py
@@ -1,23 +1,29 @@
 import numpy as np
-import pandas as pd
 from skbio.stats.composition import closure
 from .base import _BaseTransform
 import warnings
-np.seterr(all='ignore') #prevent inf errors
+np.seterr(all='ignore')
+# need to ignore log of zero warning
+
 
 class rclr(_BaseTransform):
 
     def __init__(self):
-        """ 
+        """
 
-        The rclr procedure first log transform the nonzero values before centering the data -- 
-        we refer to this preprocessing procedure as the robust center log-ratio (rclr) due 
-        to its ties to the clr transform commonly used in compositional data analysis.
-        
+        The rclr procedure first log transform
+        the nonzero values before centering the data
+        we refer to this preprocessing procedure as
+        the robust center log-ratio (rclr) due to its
+        ties to the clr transform commonly used in
+        compositional data analysis.
+
         Parameters
         ----------
-        
-        X: numpy.ndarray - a table array of all positive count data of shape (M,N) containing zeros
+
+        X: numpy.ndarray - a table array of all
+        positive count data of shape (M,N) containing zeros
+
         N = Features (i.e. OTUs, metabolites)
         M = Samples
 
@@ -28,10 +34,10 @@ class rclr(_BaseTransform):
         ------
         ValueError
 
-        Raises an error if values in array are negative 
+        Raises an error if values in array are negative
             `ValueError: Array Contains Negative Values`.
 
-        Raises an error if Data-table contains either np.inf or -np.inf   
+        Raises an error if Data-table contains either np.inf or -np.inf
             Data-table contains either np.inf or -np.inf
 
         Raises an error  Data-table contains nans
@@ -46,85 +52,86 @@ class rclr(_BaseTransform):
         ----------
         -
 
-        Examples 
+        Examples
         --------
-        
+
         >>> from deicode.optspace import OptSpace
         >>> from deicode.preprocessing import rclr
         >>> import numpy as np
-        
-        numpy.ndarray - a array of counts (samples,features) with shape (M,N) where N>M
-        >>> data=np.array([[3, 3, 0], [0, 4, 2], [3, 0, 1]]) 
+
+        numpy.ndarray - a array of counts (samples,features)
+        with shape (M,N) where N>M
+
+        >>> data=np.array([[3, 3, 0], [0, 4, 2], [3, 0, 1]])
         >>> table_rclr=rclr().fit_transform(data)
 
         """
         return
 
-    def fit(self,X):
+    def fit(self, X):
         """  fits and calc. the rclr """
-        X_=np.array(X.copy()).astype(np.float64)
-        self.X_=X_
+        X_ = np.array(X.copy()).astype(np.float64)
+        self.X_ = X_
         self._fit()
         return self
 
     def _fit(self):
         """ fits and calc. the rclr  """
 
-        X_=self.X_.copy().astype(float)
+        X_ = self.X_.copy().astype(float)
 
-        if (X_<0).any():
-            raise ValueError('Array Contains Negative Values') 
+        if (X_ < 0).any():
+            raise ValueError('Array Contains Negative Values')
 
-        if np.count_nonzero(np.isinf(X_))!=0:
-            raise ValueError('Data-table contains either np.inf or -np.inf') 
+        if np.count_nonzero(np.isinf(X_)) != 0:
+            raise ValueError('Data-table contains either np.inf or -np.inf')
 
-        if np.count_nonzero(np.isnan(X_))!=0:
-            raise ValueError('Data-table contains nans') 
+        if np.count_nonzero(np.isnan(X_)) != 0:
+            raise ValueError('Data-table contains nans')
 
-        if np.count_nonzero(X_)==0: 
-            warnings.warn(
-            "Data-table contains no zeros."
-            ,RuntimeWarning)
+        if np.count_nonzero(X_) == 0:
+            warnings.warn("Data-table contains no zeros.", RuntimeWarning)
 
-
-        X_log=np.log(closure(np.array(X_))) #log of all values
+        X_log = np.log(closure(np.array(X_)))
         log_mask = np.array(
-            [True] * X_log.shape[0] * X_log.shape[1] 
+            [True] * X_log.shape[0] * X_log.shape[1]
             ).reshape(X_log.shape)
-        log_mask[np.isfinite(X_log)] = False # convert in to zero
+        log_mask[np.isfinite(X_log)] = False
         # sum of rows (features)
         m = np.ma.array(X_log, mask=log_mask)
-        gm=m.mean(axis=-1,keepdims=True)
-        m = (m - gm).squeeze().data
-        m[~np.isfinite(X_log)]=np.nan
-        self.X_sp=m
- 
-    def fit_transform(self,X):
+        gm = m.mean(axis=-1, keepdims=True)
+        m = (m-gm).squeeze().data
+        m[~np.isfinite(X_log)] = np.nan
+        self.X_sp = m
+
+    def fit_transform(self, X):
         """ directly returns the rclr transform  """
-        X_=np.array(X.copy()).astype(np.float64)
-        self.X_=X_
+        X_ = np.array(X.copy()).astype(np.float64)
+        self.X_ = X_
         self._fit()
         return self.X_sp
+
 
 class inverse_rclr(_BaseTransform):
 
     def __init__(self):
         return
 
-    def fit(self,X):
+    def fit(self, X):
         """ TODO """
-        X_=np.array(X.copy()).astype(np.float64)
-        self.X_=X_
+        X_ = np.array(X.copy()).astype(np.float64)
+        self.X_ = X_
         self._fit()
         return self
 
     def _fit(self):
         """ TODO """
-        self.X_sp=closure(np.exp(np.array(self.X_.copy()).astype(np.float64)))
+        X_sp = np.exp(np.array(self.X_.copy()))
+        self.X_sp = closure(X_sp).astype(np.float64)
 
-    def fit_transform(self,X):
+    def fit_transform(self, X):
         """ TODO """
-        X_=np.array(X.copy()).astype(np.float64)
-        self.X_=X_
+        X_ = np.array(X.copy()).astype(np.float64)
+        self.X_ = X_
         self._fit()
         return self.X_sp
